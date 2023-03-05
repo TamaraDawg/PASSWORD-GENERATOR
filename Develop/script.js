2462350633b254991616b598c22be0f6c2e5c651
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-03-06 09:29:03 +1100

    Created Array list for all characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
+function generatePassword() {
 
+//Array of letters, uppercase and lowercase. 
+  var upperCaseLetters = [ "A", "B", "C", "D","E","F","G","H","I","J","K","L", "N", "O","P", "Q","R",
+"S","T","U","V","W", "X", "Y", "Z"];
+  var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+}
+//Array of numbers and special characters
+var numChar = ["0","1","2","3","4","5","6","7","8","9"]
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
